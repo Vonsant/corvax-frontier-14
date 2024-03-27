66faee1448d74295f366b9b4085e2ff0eb3b93ce
Zekins <136648667+Zekins3366@users.noreply.github.com>
2024-03-27 14:44:54 +0300

    Update SpeciesPrototype.cs

--- Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
+++ Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
@@ -95,9 +95,6 @@ public sealed partial class SpeciesPrototype : IPrototype
     [DataField]
     public string FemaleFirstNames { get; private set; } = "names_first_female";
 
-    [DataField]
-    public string LastNames { get; private set; } = "names_last";
-
     // Corvax-LastnameGender-Start: Split lastname field by gender
     [DataField]
     public string MaleLastNames { get; private set; } = "names_last_male";
